f169b040abcc82dede3ab3beeeba9253373f3e11
gerub <jerubaalahouangbe42@gmail.com>
2023-11-21 08:07:25 +0100

    fin

--- src/views/examples/Reglement.js
+++ src/views/examples/Reglement.js
@@ -9,10 +9,20 @@ import 'assets/css/customerDesign.css';
 
 const Users = () => {
  // const [room, setRoom] = useState([]); // Assurez-vous de déclarer l'état pour la variable room
+
+ const customTheme = {
+  text: {
+    primary: 'red',
+  },
+  background: {
+    default: 'lightblue',
+  },
+};
   const cols = [
    
     {
       name: 'NOM',
+      
       selector: (row) => row.first_name,
       sortable: true,
     },
@@ -22,20 +32,32 @@ const Users = () => {
       sortable: true,
     },
     {
-      name: 'TYPE',
+      name: 'NBRE FACTURE',
       selector: (row) => row.type,
       sortable: true,
+      
     },
     {
-      name: 'PRIX (FCFA)',
+      name: 'MONTANT IMPAYE(FCFA)',
       selector: (row) => row.price,
       sortable: true,
     },
     {
-      name: 'STATUT',
+      name: 'DETAIL',
       selector: (row) => row.statut,
       sortable: true,
     },
+
+    {
+        name: 'PAYER',
+        cell: (row) => (
+          <button onClick={() => handleFilter(row)}>PAYER</button>
+        ),
+        allowOverflow: true,
+        button: true,
+        selector: (row) => row.statut,
+        sortable: true,
+      },
   ];
   
 
@@ -48,14 +70,14 @@ const Users = () => {
 
   return (
     <div className="backgroundImgClient">
-      <Header menuTitle="UTILISATEURS" />
+      <Header menuTitle="REGLEMENTS" />
 
       <Container className="my-5" fluid>
         <div className="row">
           <div className="float-left col-md-3 col-12">
-            <AjoutUser butonTitle="Ajouter un utilisateur" />
+
           </div>
-          <div className="float-right offset-md-5 col-md-4 col-12" style={{ width: '20%', display: 'flex', justifyContent: 'right' }}>
+          <div className="float-right offset-md-5 col-md-4 col-12" style={{ width: '50%', display: 'flex', justifyContent: 'right' }}>
             <Input type="text" placeholder="Recherche..." onChange={(e) => handleFilter(e)} />
           </div>
         </div>
@@ -65,14 +87,10 @@ const Users = () => {
         <div>
           
           <div>
-            <DataTable className="" title="Liste des utilisateurs" columns={cols} data={client} keyField="id" pagination>
+            <DataTable className="" title="Liste des reglements" columns={cols} data={client} keyField="id" pagination               theme={customTheme}
+>
               {/* Ajoutez ici des composants DataTable si nécessaire */}
-              title="Liste des Chambres"
-            columns={cols}
-            data={client}
-            keyField="id"
-            pagination 
-             
+            
 
             </DataTable>
           </div>
