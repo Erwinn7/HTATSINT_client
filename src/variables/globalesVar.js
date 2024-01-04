23ad893316fa805452608320a9d95dc7b787fce4
gerub <jerubaalahouangbe42@gmail.com>
2024-01-04 12:30:54 +0100

    mise en place deconnfyyvg

--- src/variables/globalesVar.js
+++ src/variables/globalesVar.js
@@ -1,12 +1,18 @@
 
-const prefix_link = "https://426a-137-255-39-180.ngrok-free.app/"; 
+const prefix_link = "https://f4aa-137-255-39-180.ngrok-free.app/"; 
 
 
-const current_table_info = {
-    room : 0,
-    total: 0,
-    percent: 0
-}
+export const editions = [
+    {
+      label: "Edition 1",
+      path: "/edition/1",
+    },
+    {
+      label: "Edition 2",
+      path: "/edition/2",
+    },
+  ]
+  
 
 export  { prefix_link};  
 
